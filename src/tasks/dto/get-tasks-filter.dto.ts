0aa1d5301386f388427f7e12ff8a424b9d620500
NAJIB belhadj <najibbelhadj11@gmail.com>
2022-02-19 16:47:57 +0100

    Custom Pipe Validating the Task Status

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsIn, IsNotEmpty, IsOptional } from "class-validator";
 import { TaskStatuts } from "../task.model";
 
 export class GetTasksFilterDto{
+    @IsOptional()
+    @IsIn([TaskStatuts.DONE,TaskStatuts.IN_PROGRESS,TaskStatuts.OPEN])
     status: TaskStatuts;
+
+    @IsOptional()
+    @IsNotEmpty()
     search: string;
 }
---
+++ src/tasks/pipes/task-status-validation-pipe.ts
@@ -0,0 +1,24 @@
+import { BadRequestException, PipeTransform } from "@nestjs/common";
+import { TaskStatuts } from "../task.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStatuses = [
+        TaskStatuts.OPEN,
+        TaskStatuts.IN_PROGRESS,
+        TaskStatuts.DONE
+    ];
+    transform(value: any) {
+    value = value.toUpperCase();
+    if(!this.isStatusValid(value)){
+        throw new BadRequestException(`"${value}" is an invalid status`)
+    }
+     
+     return value;
+ }
+
+
+ private isStatusValid(status: any): Boolean{
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+ }
+}
